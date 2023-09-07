d3f269f40cc57e36bf8194696a7050caac66a5b4
NuclearGG <76840377+Bipraji@users.noreply.github.com>
2023-09-07 22:56:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function new_image()
 {
     
     //what color is ur BUGATTI//
-    fabric.Image.fromURL("BirthdayImage.jpg"ś, function(Img){
+    fabric.Image.fromURL("BirthdayImage.jpg", function(Img){
         hbd_obj = Img;
         hbd_obj.scaleToWidth(700);
         hbd_obj.scaleToHeight(510);
